21f4ba28ece853c01f48ae49f00d449050fe692a
Rahman <rachman.sd@gmail.com>
2019-04-22 19:50:08 +0700

    create form submit topic

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <template>
   <div id="app">
+    <reddit-view/>
   </div>
 </template>
 
 <script>
+import RedditView from './components/RedditView'
 export default {
   name: 'app',
   components: {
+    RedditView
   } 
 }
 </script>
---
+++ src/components/RedditView.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="container" style="padding-top: 40px">
+        <div class="row">
+            <div class="col-sm-5 col-sm-offset-3 col-md-6 col-md-offset-3">
+                <div class="row">
+                    <div class="col-sm-12">
+                        <div class="form-group">
+                            <input type="text" class="form-control" placeholder="title" v-model="topic.title">
+                        </div>
+                        <div class="form-group">
+                            <textarea class="form-control" rows="5" placeholder="topic content" v-model="topic.content"></textarea>
+                        </div>
+                        <button class="btn btn-info pull-right">Create Topic</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      topic: { title: '', content: '' },
+      topics: []
+    }
+  }
+}
+</script>
